17eb3277a7b65232a1d67088c7d136b458b551cb
Constantine Vassilev  <smartgolang@gmail.com>
2020-11-29 11:03:21 -0800

    go-projects_v0.1_2020-11-29-11H-03M-17S

--- embedded-static-assets/bld.bash
+++ embedded-static-assets/bld.bash
@@ -3,6 +3,8 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
+go mod init smartgolang.com/embedded-static-assets
+go mod tidy
 go version 
 
 go build -v embedded-static-assets.go
---
+++ go.mod
@@ -0,0 +1,3 @@
+module smartgolang.com/embedded-static-assets
+
+go 1.16
